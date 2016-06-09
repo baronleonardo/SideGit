c8adb6d5890b04448b35e3a7a9229d61817d77d5
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-06-09 13:48:35 +0200

    create an inhertiance add_files_btn from QPushbutton

--- addfiles_button.cpp
+++ addfiles_button.cpp
@@ -1,6 +1,42 @@
 #include "addfiles_button.h"
+#include <QDebug>
+#include <QMimeData>
+#include <QFileDialog>
+#include "git_functions.h"
+#include <cassert>
 
-AddFiles_button::AddFiles_button()
+AddFiles_button::AddFiles_button(QWidget* parent) : QPushButton(parent)
 {
+    connect( this, SIGNAL(clicked(bool)), this, SLOT(add_files_event()) );
+    repository = "";
+}
 
+//void AddFiles_button::dragEnterEvent(QDragEnterEvent *event)
+//{
+//    if( event->mimeData()->hasUrls() )
+//        event->acceptProposedAction();
+//}
+
+//void AddFiles_button::dropEvent(QDropEvent * event)
+//{
+
+//    // we use for loop as if the user drag more than one file
+//    foreach( const QUrl &url, event->mimeData()->urls() )
+//    {
+//        git_add( url.toLocalFile() );
+//    }
+//}
+
+void AddFiles_button::add_files_event()
+{
+    assert( repository != "" );
+
+    QStringList list = QFileDialog::getOpenFileNames(this,
+        tr("Choose file(s) to be added to the current repo"),
+        repository, "*");
+
+    foreach (QString file_name, list)
+    {
+        git_add( file_name );
+    }
 }
--- addfiles_button.h
+++ addfiles_button.h
@@ -1,11 +1,26 @@
 #ifndef ADDFILES_BUTTON_H
 #define ADDFILES_BUTTON_H
 
+#include <QPushButton>
+#include <QDropEvent>
+#include <QString>
+#include <QList>
 
 class AddFiles_button : public QPushButton
 {
+    Q_OBJECT
+
 public:
-    AddFiles_button();
+    QString repository;
+
+public:
+    AddFiles_button(QWidget* parent = NULL);
+
+private slots:
+    void add_files_event();
+//    void dragEnterEvent(QDragEnterEvent *event);
+//    void dropEvent(QDropEvent* event);
+//    QList<QString> get_draged_files();
 };
 
 #endif // ADDFILES_BUTTON_H
