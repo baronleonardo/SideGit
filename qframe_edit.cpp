a428b5f5b0483b1202d0ca9287d3c9a23d1122f3
Baron Leonardo <mohamedayman.fcis@zoho.com>
2016-06-09 13:55:31 +0200

    create an inherited qframe-edit class from QFrame

--- qframe_edit.cpp
+++ qframe_edit.cpp
@@ -1,6 +1,28 @@
 #include "qframe_edit.h"
+#include <QMimeData>
+#include <QDragEnterEvent>
+#include <QDebug>
+#include "git_functions.h"
 
-QFrame_edit::QFrame_edit()
+QFrame_edit::QFrame_edit(QWidget* parnet) : QFrame(parnet)
 {
 
+}
+
+void QFrame_edit::dragEnterEvent(QDragEnterEvent *event)
+{
+    if( event->mimeData()->hasUrls() )
+    {
+        event->acceptProposedAction();
+        emit attempt_to_drop();
+    }
+}
+
+void QFrame_edit::dropEvent(QDropEvent * event)
+{
+    // we use for loop as if the user drag more than one file
+    foreach( const QUrl &url, event->mimeData()->urls() )
+    {
+        git_add( url.toLocalFile() );
+    }
 }
--- qframe_edit.h
+++ qframe_edit.h
@@ -1,11 +1,21 @@
 #ifndef QFRAME_EDIT_H
 #define QFRAME_EDIT_H
 
+#include <QObject>
+#include <QFrame>
 
 class QFrame_edit : public QFrame
 {
+    Q_OBJECT
 public:
-    QFrame_edit();
+    QFrame_edit(QWidget* parnet = NULL);
+
+public slots:
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent* event);
+
+signals:
+    void attempt_to_drop();
 };
 
 #endif // QFRAME_EDIT_H
